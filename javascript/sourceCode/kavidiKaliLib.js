26e4f4381e58538eefe1514acf480584997d73d9
sumanm <sumanm@thoughtworks.com>
2015-11-21 12:32:57 +0530

    initial commit with package.json

---
+++ javascript/sourceCode/kavidiKaliLib.js
@@ -0,0 +1,27 @@
+var lodash = require('lodash');
+var entities = {};
+exports.entities = entities;
+
+entities.Board = function(){
+
+};
+
+entities.Coin = function(){
+
+};
+
+entities.Player = function(){
+
+};
+
+entities.Dice = function(){
+
+};
+
+entities.GameMaster = function(){
+
+};
+
+entities.SafePlaces = function(){
+
+}
