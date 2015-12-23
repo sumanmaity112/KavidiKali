ab49d2f47bd750d289afd8f8b705812e758e91d5
Supriya Gole <supriyagole12@gmail.com>
2015-12-23 18:46:52 +0530

    [supriya] created test folder to test server
    What changes have you made?
    
    *
    
    Why do we need this change?
    
    * I have commited this changes for writing  parallel tests.
    
    What can this change impact?
    
    * Nothing.

---
+++ server/test/serverTest.js
@@ -0,0 +1,26 @@
+var request = require('superTest');
+var routes = require('../routing.js');
+
+// describe("serve static file",function(){
+// 	it("serves index file if '/' is given",function(done){
+// 		request(routes)
+// 		.get('/')
+// 		.expect(200)
+// 		.expect(/Welcome To KavidiKali/)
+// 		.expect('content-Type',/text\/html/,done);
+// 	});	
+	// it("serves index file index.html is requested",function(done){
+	// 	request(routes)
+	// 	.get('/index.html')
+	// 	.expect(200)
+	// 	.expect(/Welcome To KavidiKali/)
+	// 	.expect('content-Type',/text\/html/,done);
+	// });
+	// it("serves the main.html when requested",function(done){
+	// 	request(routes)
+	// 	.get('/main.html')
+	// 	.expect(200)
+	// 	.expect(/Welcome To KavidiKali/)
+	// 	.expect('content-Type',/text\/html/,done);
+	// });
+//});
