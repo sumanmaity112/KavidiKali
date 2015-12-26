7153338f3da6c6756f5c7bef3fc660f54110afa9
sumanmaity112 <suman.maity112@gmail.com>
2015-12-27 00:17:54 +0530

    [Suman]-Add test for currentPlayer url
    What changes have you made?
    
    * Added test for /enquiry/question=currentPlayer
    
    Why do we need this change?
    
    * For test the funtionality of /enquiry/question=currentPlayer we need
    * these changes
    
    What can this change impact?
    
    * Nothing

--- server/test/serverTest.js
+++ server/test/serverTest.js
@@ -56,6 +56,14 @@ describe("get handlers",function(){
 				.expect(/Not Found/,done)
 		});
 	});
+	describe(" ",function(){
+		it("gives method not allowed when the method is other than GET or POST",function(done){
+			request(controller)
+				.put('/anything')
+				.expect(405)
+				.expect('Method is not allowed',done);
+		});
+	});
 	describe("main.html",function(){
 		it("redirects to index page when player is unregistered",function(done){
 			request(controller)
@@ -63,7 +71,39 @@ describe("get handlers",function(){
 				.expect(302)
 				.expect('Location','/index.html',done)
 		});
+		describe("currentPlayer",function(){
+			it("gives the name of current player when it get request from registered player",function(done){
+				game={};
+				game.players={rocky:{},jacky:{},joy:{},rony:{}};
+				game.createPlayer = function(){};
+				game.currentPlayer = 'rony'
+
+				controller = requestHandler(game);
+				request(controller)
+					.get('/enquiry/question=currentPlayer')
+					.set('cookie',['userId=rony'])
+					.expect(200)
+					.expect('rony',done);
+			});
+			it("gives the status code 404 when it get request from unregistered player",function(done){
+				game={};
+				game.players={rocky:{},jacky:{},joy:{},rony:{}};
+				game.createPlayer = function(){};
+				game.currentPlayer = 'rony'
+
+				controller = requestHandler(game);
+				request(controller)
+					.get('/enquiry/question=currentPlayer')
+					.set('cookie',['userId=roy'])
+					.expect(404)
+					.expect('Not Found',done);
+			});
+			
+		});
 	});
+});
+
+describe("POST handlers",function(){
 	describe("index page",function(){
 		it("redirects player to the waiting page after login from url /",function(done){
 			game={players:{}};
@@ -72,10 +112,12 @@ describe("get handlers",function(){
 			controller = requestHandler(game);
 			request(controller)
 				.post('/')
-				.send("name=Suman")
+				.send("name=rony")
+				.expect('set-cookie','userId=rony')
 				.expect(302)
 				.expect('Location','/waitingPage.html',done);
 		});
+
 		it("redirects player to the waiting page after login from url /indexPage.html",function(done){
 			game={players:{}};
 			game.createPlayer=function(){};
@@ -86,28 +128,22 @@ describe("get handlers",function(){
 				.expect(302)
 				.expect('Location','/waitingPage.html',done);
 		});
-	});
-	describe(" ",function(){
-		it("gives method not allowed when the method is other than GET or POST",function(done){
+
+		it("gives a waiting message when more than 4th player want to join the game",function(done){
+			game={};
+			game.players={rocky:{},jacky:{},joy:{},johnny:{}};
+			game.createPlayer=function(){};
+			controller = requestHandler(game);
 			request(controller)
-				.put('/anything')
-				.expect(405)
-				.expect('Method is not allowed',done);
+				.post('/').send('name=john')
+				.expect(200)
+				.expect("Please wait, a game is already started",done);
 		});
 	});
-});
 
-describe("main.html",function(){
-	it("gives a waiting message when more than 4th player want to join the game",function(done){
-		game={};
-		game.players={rocky:{},jacky:{},joy:{},johnny:{}};
-		game.createPlayer=function(){};
-		controller = requestHandler(game);
-		request(controller)
-			.post('/').send('name=john')
-			.expect(200)
-			.expect("Please wait, a game is already started",done);
-	});
+	describe("main.html",function(){
+
+	});	
 });
 
 
