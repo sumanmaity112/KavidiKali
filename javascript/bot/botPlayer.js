85e4926bacc95b07d5818c82b196a28ffd9a22b3
Supriya Gole <supriyagole12@gmail.com>
2016-02-23 21:56:14 +0530

    [Suman/Supriya]-Bot player can roll dice
    What changes have you made?
    
    *Update bot player which can roll a dice on its turn
    
    Why do we need this change?
    
    *Bot player should able to roll a dice
    
    What can this change impact?
    
    *Everything working fine except bot player. But till it is in W.I.P

--- javascript/bot/botPlayer.js
+++ javascript/bot/botPlayer.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var chance = new require('chance')();
-var emitter = new (require('events').EventEmitter)();
+var EventEmitter = require('events').EventEmitter;
 
 const HOST = process.env.BOT_HOST || 'localhost';
 const PORT = process.env.BOT_PORT || 8080;
@@ -13,36 +13,84 @@ var createHttpOption = function(url,method,cookie){
       path: url,
       method: method,
       headers: {
-          'Cookie': cookie,
-          'Content-Type': 'application/x-www-form-urlencoded'
+          'cookie': cookie,
+          'content-type': 'application/x-www-form-urlencoded'
       }
     };
     return options;
 }
 
 var botPlayer = function(gameId){
-  console.log('---- in bot');
+  var self = this;
   this.name = chance.first();
   this.http = http;
   this.gameId = gameId;
+  this.cookie = createCookie(this.name,gameId);
+  this.timer = setInterval(function(){
+                  console.log("Hey i am   ");
+                  botManager(self);
+               },1000);
+  this.emitter = new EventEmitter();
 }
 
 botPlayer.prototype = {
   start : function(){
     var self = this;
     var data = querystring.stringify({
-          name: 'botPlayer',
+          name: self.name,
           gameId: self.gameId,
           option: 'joinGame'
         });
-    var option=createHttpOption('/login','POST',self.name);
+    var option=createHttpOption('/login','POST',self.cookie);
     var req = self.http.request(option,function(res){
-      console.log(res.statusCode,"*****   ___")
     });
     req.write(data);
     req.end();
-  },
 
+    self.emitter.addListener('rollDice',function(){
+      rollDice(self);
+    });
 
+    self.emitter.addListener('moveCoin',function(){
+      moveCoin(self);
+    });
+  }
+}
+
+var rollDice = function(self){
+  var option=createHttpOption('/instruction?action=rollDice','GET',self.cookie);
+  var req = self.http.get(option,function(res){
+    moveCoin(self);
+  });
+}
+
+var moveCoin = function(self){
+  var option=createHttpOption('/instruction?action=moveCoin','GET',self.cookie);
+  var req = self.http.get(option,function(res){
+    console.log("yeah!!!!!!!!!!i came here");
+  });
+}
+
+var botManager = function(self){
+  var option = createHttpOption('/enquiry?question=currentPlayer','GET',self.cookie);
+  self.http.get(option,function(res){
+    res.on('data',function(chunk){
+      if(chunk.toString() == self.name){
+        self.emitter.emit('rollDice');
+        var playerChance = createHttpOption('/enquiry?question=anyMoreChances','GET',self.cookie);
+        self.http.get(playerChance,function(res){
+          res.on('data',function(chunk){
+            console.log("=========chances",chunk.toString());
+            if(chunk.toString()=='true')
+              self.emitter.emit('moveCoin');
+          });
+        });
+      }
+    });
+  });
+}
+
+var createCookie = function(userId,gameId){
+  return "userId="+userId+"; gameId="+gameId;
 }
 module.exports = botPlayer;
