6131470489d433e45aadeb16fc8257d06b5c783c
sumanmaity112 <suman.maity112@gmail.com>
2016-02-29 00:03:17 +0530

    [Suman]- New modification in bot player
    What changes have you made?
    
    *	Modify the bot player's coin choosing logic. Bot player can take
    * some decision when to choose specific coin depends on game status.
    
    Why do we need this change?
    
    *	Bot player should able to take decision about choose a 	coin to kill
    * opponents coin.
    
    What can this change impact?
    
    *	Now bot player is semi-intelligent, but it is till in W.I.P

--- javascript/bot/chooseCoin.js
+++ javascript/bot/chooseCoin.js
@@ -12,35 +12,21 @@ var findMyDetails = function(option,self){
 };
 
 var chooseCoin = function(myGameStatus,self,option){
-	var coins = myGameStatus.coins;
-	console.log(myGameStatus.diceValues.length,"******************",myGameStatus.diceValues);
-	if(myGameStatus.diceValues.indexOf(6)!=-1) {
-		var coinsEligibleToEnter = getOffBoardCoins(coins);
-		console.log(coinsEligibleToEnter);
-		var selectedCoin = coinsEligibleToEnter[0];
-		option.path = '/instruction?action=moveCoin&coin=' + selectedCoin.id +'&position='+ myGameStatus.path[0].id; 
-		moveCoin(option);
-		// selectedCoin.currentPosition = myGameStatus.path[0].id;
-	}
-	else{
-		var coinsEligibleToMove = getOnBoardCoins(coins);
-		if(coinsEligibleToMove.length && myGameStatus.diceValues.length){
-			console.log(coinsEligibleToMove);
-			var selectedCoin = coinsEligibleToMove[0];
-			var coinsCurrentIndex = lodash.findIndex(myGameStatus.path,{id:selectedCoin.currentPosition});
-			var coinsNextIndex = (coinsCurrentIndex+myGameStatus.diceValues[0]) % myGameStatus.path.length;
-			console.log("-- in else",coinsCurrentIndex,"       ",selectedCoin.currentPosition,"            ",coinsCurrentIndex+myGameStatus.diceValues[0]);
-			console.log(myGameStatus.path[coinsCurrentIndex+myGameStatus.diceValues[0]],"**^^^^")
-			option.path = '/instruction?action=moveCoin&coin=' + selectedCoin.id +'&position='+ myGameStatus.path[coinsNextIndex].id;
+	option.path='/update?toUpdate=board';
+	http.get(option,function(res){
+		res.on('data',function(metaData){
+			var availableCoin = chooseBestCoinToMove(JSON.parse(metaData),myGameStatus,myGameStatus.diceValues[0]);
+			option.path = '/instruction?action=moveCoin&coin=' + availableCoin.coinId +'&position='+ availableCoin.tileId;
 			moveCoin(option);
-		}
-	}
-	if(myGameStatus.diceValues.length!=0)
-		setTimeout(findMyDetails(option,self),10000);
+			if(myGameStatus.diceValues.length>1)
+				findMyDetails(option,self);
+		})
+	})
+	
 }
 
 var moveCoin = function(option){
-	console.log(option.path);
+	console.log(option.path,"**^^^");
 	http.get(option,function(res){});
 }
 
@@ -52,8 +38,73 @@ var getOffBoardCoins = function(coins){
 
 var getOnBoardCoins = function(coins){
 	return lodash.filter(coins,function(coin){
-		return coin.currentPosition!=-1;
+		return coin.currentPosition!=-1 && coin.currentPosition!='2,2';
 	});
 }
+var chooseBestCoinToMove=function(stateOfTheGame,myGameStatus,diceValue){
+	var path = myGameStatus.path;
+	var coins = myGameStatus.coins;
+	var myCoinColor = 'green';
+	var opponentsCoins = lodash.filter(stateOfTheGame,function(coin){return coin.colour!=myCoinColor});
+	// console.log(opponentsCoins);
 
+
+
+
+	var coinsEligibleToEnter = getOffBoardCoins(coins);
+	
+
+	if(myGameStatus.diceValues.indexOf(6)!=-1 && coinsEligibleToEnter.length) {
+		return {coinId:coinsEligibleToEnter[0].id,tileId:path[0].id};
+	}
+	else{
+		var coinsEligibleToMove = getOnBoardCoins(coins);
+		if(coinsEligibleToMove.length && myGameStatus.diceValues.length){
+			var result = chooseValidCoin(myGameStatus,coinsEligibleToMove,diceValue,opponentsCoins);
+			return result;
+		}
+	}
+	return {};
+
+}
+var chooseValidCoin=function(myGameStatus,coinsEligibleToMove,diceValue,opponentsCoins){
+	if(!coinsEligibleToMove.length)
+		return {};
+	for(var coin in coinsEligibleToMove){
+		var coinsCurrentIndex = lodash.findIndex(myGameStatus.path,{id:coinsEligibleToMove[coin].currentPosition});
+		// var coinsNextIndex = (coinsCurrentIndex+diceValue) % myGameStatus.path.length;
+		var coinsNextIndex = myGameStatus.path.length<=16 ? ((coinsCurrentIndex+diceValue) % myGameStatus.path.length): (coinsCurrentIndex+diceValue);
+		if(!myGameStatus.path[coinsNextIndex])
+			continue;
+		var tileId = myGameStatus.path[coinsNextIndex].id;
+		for(var opponentsCoin in opponentsCoins){
+			if(tileId==opponentsCoins[opponentsCoin].currentPosition){
+				return {coinId:coinsEligibleToMove[coin].id,tileId:tileId};
+			}
+		}
+	}
+	for (var i = 0; i < coinsEligibleToMove.length; i++) {
+		var selectedCoin = coinsEligibleToMove[i];
+		var coinsCurrentIndex = lodash.findIndex(myGameStatus.path,{id:selectedCoin.currentPosition});
+
+		var coinsNextIndex = myGameStatus.path.length<=16 ? ((coinsCurrentIndex+diceValue) % myGameStatus.path.length): (coinsCurrentIndex+diceValue);
+		var tile = myGameStatus.path[coinsNextIndex];
+		if(!tile)
+			continue;
+		console.log(!isMyCoinPresent(myGameStatus.coins,tile),'--------------')
+		
+		if(!isMyCoinPresent(myGameStatus.coins,tile)){
+			console.log({coinId:selectedCoin.id,tileId:tile.id},"*****")
+			return {coinId:selectedCoin.id,tileId:tile.id};
+		}
+	}
+	return {};
+}
+var isMyCoinPresent=function(coins,tile){
+	for(var counter = 0;counter<coins.length;counter++){
+		if(coins[counter].currentPosition == tile.currentPosition && !(tile.coins instanceof Array))
+			return true;
+	}
+	return false;
+}
 module.exports = findMyDetails;
