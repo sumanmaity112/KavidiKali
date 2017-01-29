1e88d790dc018f9ab254c1f6879220b51fa3b3bf
sumanmaity112 <suman.maity112@gmail.com>
2017-01-29 18:10:44 +0530

    Revert "[605142d] Sooraj: refactoring player entity"
    This reverts commit 605142d44f6b82588db0826204694f0b8f94e84a.

--- javascript/sourceCode/coin.js
+++ javascript/sourceCode/coin.js
@@ -1,36 +1,31 @@
-const INITIAL_POSITION = -1,
-    DESTINATION = '2,2';
-var EventEmitter = require('events').EventEmitter;
+var EventEmitter=require('events').EventEmitter;
 
-var Coin = function(id, colour) {
-    this.id = id;
-    this.currentPosition = INITIAL_POSITION;
-    this.reachedDestination = false;
-    this.colour = colour;
-    this.emitter = new EventEmitter();
+var Coin = function(id,colour){
+	this.id = id;
+	this.currentPosition = -1 ;
+	this.reachedDestination = false;
+	this.colour = colour;
+	this.emitter = new EventEmitter();
 };
 
 Coin.prototype = {
-    addListener: function(listener) {
-        this.emitter.addListener("have_killed", listener.whenCoinKills.bind(listener));
-    },
-    equals: function(other) {
-        return this.colour == other.colour;
-    },
-    move: function(movesTo) {
-        this.currentPosition = movesTo;
-        if (this.currentPosition == DESTINATION)
-            this.reachedDestination = true;
-    },
-    kill: function() {
-        this.move(INITIAL_POSITION);
-    },
-    killed: function() {
-        this.emitter.emit("have_killed");
-    },
-    isAtBeach: function() {
-        return this.currentPosition == INITIAL_POSITION;
-    }
+	addListener:function(listener) {
+		this.emitter.addListener("have_killed",listener.whenCoinKills.bind(listener));
+	},
+	equals:function(other) {
+		return this.colour==other.colour;
+	},
+	move:function(movesTo) {
+		this.currentPosition = movesTo;
+		if(this.currentPosition=='2,2')
+			this.reachedDestination=true;
+	},
+	kill:function() {
+		this.move(-1);	
+	},
+	killed:function() {
+		this.emitter.emit("have_killed");
+	}
 };
 
 module.exports = Coin;
